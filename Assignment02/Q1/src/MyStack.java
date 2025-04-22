b9b3627625dddb389a5fa90fddaa47c1ec833b35
Pham Bao Phi <baophi852@gmail.com>
2025-04-22 08:46:07 +0700

    do q1 and q2 of assignment 2

---
+++ Assignment02/Q1/src/MyStack.java
@@ -0,0 +1,121 @@
+
+import java.util.*;
+import java.io.*;
+
+public class MyStack {
+
+    Node head;
+    int size;
+
+    MyStack() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public void clear() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    void ftraverse(RandomAccessFile f) throws Exception {
+        Node p = head;
+        while (p != null) {
+            f.writeBytes(p.info.name + "-" + p.info.price + "     "); // write data in the node p to the file f
+            p = p.next;
+        }
+
+        f.writeBytes("\r\n");
+    }
+
+    void loadData(int k) {
+        String[] a = Lib.readLineToStrArray("data.txt", k);
+        String[] b = Lib.readLineToStrArray("data.txt", k + 1);
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int p = Integer.parseInt(b[i]);
+
+            push(a[i], p);
+        }
+    }
+
+    void push(String n, float p) {
+        //------------------------------------------------------------------------------------
+        //------ Start your code here---------------------------------------------------------
+        Phone newP = new Phone(n, p);
+        Node newN = new Node(newP);
+        newN.next = this.head;
+        this.head = newN;
+        size++;
+        //------ End your code here-----------------------------------------------------------
+        //------------------------------------------------------------------------------------
+    }
+
+    // f1: ham nay se goi ham push nhieu lan
+    void f1() throws Exception {
+        clear();
+        loadData(0);
+        String fname = "f1.txt";
+        File g123 = new File(fname);
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
+        ftraverse(f);
+        f.close();
+    }
+
+    void f2() throws Exception {
+        clear();
+        loadData(0);
+        String fname = "f2.txt";
+        File g123 = new File(fname);
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
+        //------------------------------------------------------------------------------------
+        //------ Start your code here---------------------------------------------------------
+        if (!isEmpty()) {
+            Phone value = this.head.info;
+            f.writeBytes(value.toString() + "\n");
+            this.head = this.head.next;
+            size--;
+        } else {
+            f.writeBytes(null + "\n");
+        }
+
+        //------ End your code here-----------------------------------------------------------
+        //------------------------------------------------------------------------------------
+        ftraverse(f);
+        f.close();
+    }
+
+    void f3() throws Exception {
+        clear();
+        loadData(0);
+        String fname = "f3.txt";
+        File g123 = new File(fname);
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
+
+        //------------------------------------------------------------------------------------
+        //------ Start your code here---------------------------------------------------------
+        if (!isEmpty()) {
+            Phone value = this.head.info;
+            f.writeBytes(value.toString() + "\n");
+        } else {
+            f.writeBytes(null + "\n");
+        }
+        //------ End your code here-----------------------------------------------------------
+        //------------------------------------------------------------------------------------
+        ftraverse(f);
+        f.close();
+    }
+
+}
